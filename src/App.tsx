21dbe1468bfd2cd99b99eb91091cf61f39ede0b9
yamsunsee <yamsunsee@gmail.com>
2026-03-27 11:47:33 +0700

    🐸

--- src/App.tsx
+++ src/App.tsx
@@ -20,18 +20,18 @@ import { randomScrambleForEvent } from "https://cdn.cubing.net/v0/js/cubing/scra
 const App = () => {
   const [cube, setCube] = useState<number[][]>();
   const [scrambleString, setScrambleString] = useState<string>("");
-  const [labels, setLabels] = useState<string[]>(["corners", "edges"]);
+  const [solvedCornerString, setSolvedCornerString] = useState<string>("");
+  const [solvedEdgeString, setSolvedEdgeString] = useState<string>("");
+  const [labels, setLabels] = useState<string[]>([]);
   const [colors, setColors] = useState<string[]>(["corners", "edges"]);
+  const [isShowSolvedCircles, setIsShowSolvedCircles] =
+    useState<boolean>(false);
 
-  useEffect(() => {
-    const generateScramble = async () => {
-      const generatedScramble = await randomScrambleForEvent("333");
-      const newScrambleString = generatedScramble.toString();
-      setScrambleString(newScrambleString);
-    };
-
-    generateScramble();
-  }, []);
+  const generateScramble = async () => {
+    const generatedScramble = await randomScrambleForEvent("333");
+    const newScrambleString = generatedScramble.toString();
+    setScrambleString(newScrambleString);
+  };
 
   useEffect(() => {
     const newCube = applyAlgorithm(FACE_STICKER_IDS, scrambleString);
@@ -51,19 +51,94 @@ const App = () => {
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-zinc-950 p-4">
-      <input
-        autoFocus
-        value={scrambleString}
-        onChange={(event) =>
-          setScrambleString(event.target.value.toUpperCase())
-        }
-        className="w-full rounded-lg border border-zinc-800 bg-zinc-900 p-4 text-center text-4xl font-bold text-white outline-none"
-        placeholder="Enter scramble..."
-      />
-      {scrambleString && (
-        <div className="grid w-full flex-1 gap-4 md:grid-cols-[2fr_1fr_1fr]">
-          <div className="flex flex-col justify-between gap-4">
-            <div className="grid grid-cols-4 gap-2">
+      <div className="flex w-full flex-col gap-2">
+        <div className="flex items-center gap-2">
+          <input
+            value={scrambleString}
+            onChange={(event) =>
+              setScrambleString(event.target.value.toUpperCase())
+            }
+            className="w-full rounded-lg border border-zinc-800 bg-zinc-900 p-4 text-center text-4xl font-bold text-white outline-none"
+            placeholder="SCRAMBLE"
+          />
+          <button
+            onClick={async () => {
+              await generateScramble();
+              setIsShowSolvedCircles(false);
+              setLabels([]);
+              setSolvedCornerString("");
+              setSolvedEdgeString("");
+            }}
+            className="w-60 rounded-lg border border-zinc-800 bg-zinc-900 p-4 text-center text-4xl font-bold text-white uppercase outline-none"
+          >
+            Generate
+          </button>
+        </div>
+        <div className="flex items-center gap-2">
+          <div className="grid w-full grid-cols-2 gap-2">
+            <input
+              value={solvedCornerString}
+              onChange={(event) =>
+                setSolvedCornerString(
+                  event.target.value
+                    .toUpperCase()
+                    .replace(/ /g, "")
+                    .split("")
+                    .join(" "),
+                )
+              }
+              className={cn(
+                "w-full rounded-lg border border-zinc-800 bg-zinc-900 p-4 text-center text-4xl font-bold text-white outline-none",
+                isShowSolvedCircles &&
+                  (solvedCornerString.replace(/ /g, "") ===
+                  solvedCornerCircles?.replace(/ /g, "")
+                    ? "border-emerald-500 bg-emerald-950 text-emerald-500"
+                    : "border-rose-500 bg-rose-950 text-rose-500"),
+              )}
+              placeholder="CORNERS"
+            />
+            <input
+              value={solvedEdgeString}
+              onChange={(event) =>
+                setSolvedEdgeString(
+                  event.target.value
+                    .toUpperCase()
+                    .replace(/ /g, "")
+                    .split("")
+                    .join(" "),
+                )
+              }
+              className={cn(
+                "w-full rounded-lg border border-zinc-800 bg-zinc-900 p-4 text-center text-4xl font-bold text-white outline-none",
+                isShowSolvedCircles &&
+                  (solvedEdgeString.toLowerCase().replace(/ /g, "") ===
+                  solvedEdgeCircles?.replace(/ /g, "")
+                    ? "border-emerald-500 bg-emerald-950 text-emerald-500"
+                    : "border-rose-500 bg-rose-950 text-rose-500"),
+              )}
+              placeholder="EDGES"
+            />
+          </div>
+          <button
+            onClick={() => {
+              setIsShowSolvedCircles(!isShowSolvedCircles);
+              setLabels(isShowSolvedCircles ? [] : ["corners", "edges"]);
+            }}
+            className="w-60 rounded-lg border border-zinc-800 bg-zinc-900 p-4 text-center text-4xl font-bold text-white uppercase outline-none"
+          >
+            {isShowSolvedCircles ? "Hide" : "Show"}
+          </button>
+        </div>
+      </div>
+      <div
+        className={cn(
+          "grid w-full flex-1 gap-4",
+          isShowSolvedCircles && "2xl:grid-cols-[2fr_1fr_1fr]",
+        )}
+      >
+        <div className="flex flex-col justify-between gap-4">
+          <div className="flex h-full items-center justify-center">
+            <div className="grid w-fit grid-cols-4 gap-2">
               {CUBE_FACE_IDS.map((layer) => {
                 return layer.map((face, faceIndex) => {
                   if (face === null)
@@ -91,7 +166,7 @@ const App = () => {
                           <div
                             key={`sticker-${stickerIndex}`}
                             className={cn(
-                              "flex aspect-square items-center justify-center rounded-md p-1.5 text-2xl font-bold text-zinc-950 uppercase",
+                              "flex size-16 items-center justify-center rounded-md p-1.5 text-2xl font-bold text-zinc-950 uppercase",
                               !stickerLabel && stickerBackgroundColor,
                               colors.includes("corners")
                                 ? stickerLabel === stickerLabel.toUpperCase() &&
@@ -129,108 +204,114 @@ const App = () => {
                 });
               })}
             </div>
-            <div className="grid grid-cols-2 gap-4">
-              <div className="flex flex-col gap-2 rounded-lg border border-zinc-800 bg-zinc-900 p-4">
-                <div className="text-2xl font-bold text-emerald-500 uppercase">
-                  Labels
-                </div>
-                <div className="grid grid-cols-2 gap-2">
-                  {["corners", "edges"].map((option) => (
-                    <button
-                      key={`labels-${option}`}
-                      onClick={() => {
-                        if (labels.includes(option))
-                          setLabels(labels.filter((item) => item !== option));
-                        else setLabels([...labels, option]);
-                      }}
-                      className={cn(
-                        "cursor-pointer rounded-md bg-zinc-800 px-4 py-2 font-bold text-white uppercase",
-                        labels.includes(option) && "bg-white text-zinc-950",
-                      )}
-                    >
-                      {option}
-                    </button>
-                  ))}
-                </div>
-              </div>
-              <div className="flex flex-col gap-2 rounded-lg border border-zinc-800 bg-zinc-900 p-4">
-                <div className="text-2xl font-bold text-amber-500 uppercase">
-                  Colors
-                </div>
-                <div className="grid grid-cols-2 gap-2">
-                  {["corners", "edges"].map((option) => (
-                    <button
-                      key={`highlight-${option}`}
-                      onClick={() => {
-                        if (colors.includes(option))
-                          setColors(colors.filter((item) => item !== option));
-                        else setColors([...colors, option]);
-                      }}
-                      className={cn(
-                        "cursor-pointer rounded-md bg-zinc-800 px-4 py-2 font-bold text-white uppercase",
-                        colors.includes(option) && "bg-white text-zinc-950",
-                      )}
-                    >
-                      {option}
-                    </button>
-                  ))}
-                </div>
-              </div>
-            </div>
           </div>
-          <div className="flex h-full flex-col gap-2 rounded-lg border border-zinc-800 bg-zinc-900 p-4 text-2xl font-bold">
-            <div className="text-emerald-500 uppercase">Edges</div>
-            <div>
-              {solvedEdgeCircles
-                ?.replace(/\s/g, "")
-                .split("")
-                .map((item, index) => (
-                  <div
-                    key={`edge-${item}-${index}`}
+          <div className="grid grid-cols-2 gap-4">
+            <div className="flex flex-col gap-2 rounded-lg border border-zinc-800 bg-zinc-900 p-4">
+              <div className="text-2xl font-bold text-emerald-500 uppercase">
+                Labels
+              </div>
+              <div className="grid grid-cols-2 gap-2">
+                {["corners", "edges"].map((option) => (
+                  <button
+                    key={`labels-${option}`}
+                    onClick={() => {
+                      if (labels.includes(option))
+                        setLabels(labels.filter((item) => item !== option));
+                      else setLabels([...labels, option]);
+                    }}
                     className={cn(
-                      "grid grid-cols-[2rem_1fr]",
-                      index % 2 && "mb-4",
+                      "cursor-pointer rounded-md bg-zinc-800 px-4 py-2 font-bold text-white uppercase",
+                      labels.includes(option) && "bg-white text-zinc-950",
                     )}
                   >
-                    <span
-                      className={cn(
-                        "uppercase",
-                        "cwisy".includes(item) && index % 2
-                          ? "text-rose-500"
-                          : "text-sky-500",
-                      )}
-                    >
-                      {item}
-                    </span>{" "}
-                    <span className="text-white">{ALGORITHMS.EDGES[item]}</span>
-                  </div>
+                    {option}
+                  </button>
                 ))}
+              </div>
             </div>
-          </div>
-          <div className="flex h-full flex-col gap-2 rounded-lg border border-zinc-800 bg-zinc-900 p-4 text-2xl font-bold">
-            <div className="text-amber-500 uppercase">Corners</div>
-            <div>
-              {solvedCornerCircles
-                ?.replace(/\s/g, "")
-                .split("")
-                .map((item, index) => (
-                  <div
-                    key={`corner-${item}-${index}`}
+            <div className="flex flex-col gap-2 rounded-lg border border-zinc-800 bg-zinc-900 p-4">
+              <div className="text-2xl font-bold text-amber-500 uppercase">
+                Colors
+              </div>
+              <div className="grid grid-cols-2 gap-2">
+                {["corners", "edges"].map((option) => (
+                  <button
+                    key={`highlight-${option}`}
+                    onClick={() => {
+                      if (colors.includes(option))
+                        setColors(colors.filter((item) => item !== option));
+                      else setColors([...colors, option]);
+                    }}
                     className={cn(
-                      "grid grid-cols-[2rem_1fr]",
-                      index % 2 && "mb-4",
+                      "cursor-pointer rounded-md bg-zinc-800 px-4 py-2 font-bold text-white uppercase",
+                      colors.includes(option) && "bg-white text-zinc-950",
                     )}
                   >
-                    <span className="text-sky-500 uppercase">{item}</span>{" "}
-                    <span className="text-white">
-                      {ALGORITHMS.CORNERS[item]}
-                    </span>
-                  </div>
+                    {option}
+                  </button>
                 ))}
+              </div>
             </div>
           </div>
         </div>
-      )}
+        {isShowSolvedCircles && (
+          <>
+            <div className="flex h-full flex-col gap-2 rounded-lg border border-zinc-800 bg-zinc-900 p-4 text-2xl font-bold">
+              <div className="text-emerald-500 uppercase">Edges</div>
+              <div>
+                {solvedEdgeCircles
+                  ?.replace(/\s/g, "")
+                  .split("")
+                  .map((item, index) => (
+                    <div
+                      key={`edge-${item}-${index}`}
+                      className={cn(
+                        "grid grid-cols-[2rem_1fr]",
+                        index % 2 && "mb-4",
+                      )}
+                    >
+                      <span
+                        className={cn(
+                          "uppercase",
+                          "cwisy".includes(item) && index % 2
+                            ? "text-rose-500"
+                            : "text-sky-500",
+                        )}
+                      >
+                        {item}
+                      </span>{" "}
+                      <span className="text-white">
+                        {ALGORITHMS.EDGES[item]}
+                      </span>
+                    </div>
+                  ))}
+              </div>
+            </div>
+            <div className="flex h-full flex-col gap-2 rounded-lg border border-zinc-800 bg-zinc-900 p-4 text-2xl font-bold">
+              <div className="text-amber-500 uppercase">Corners</div>
+              <div>
+                {solvedCornerCircles
+                  ?.replace(/\s/g, "")
+                  .split("")
+                  .map((item, index) => (
+                    <div
+                      key={`corner-${item}-${index}`}
+                      className={cn(
+                        "grid grid-cols-[2rem_1fr]",
+                        index % 2 && "mb-4",
+                      )}
+                    >
+                      <span className="text-sky-500 uppercase">{item}</span>{" "}
+                      <span className="text-white">
+                        {ALGORITHMS.CORNERS[item]}
+                      </span>
+                    </div>
+                  ))}
+              </div>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), basicSsl()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
-    https: true,
   },
 });
